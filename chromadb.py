57adb485dd05213086ef88c5eeac3191c561d2f0
devopssii <133768745+devopssii@users.noreply.github.com>
2023-06-15 00:45:01 +0500

    Update chromadb.py

--- chromadb.py
+++ chromadb.py
@@ -1,1 +1,12 @@
+class ChromaDB:
+    def __init__(self):
+        # Инициализация соединения с базой данных ChromaDB
+        pass
 
+    def insert_product_card(self, product_card):
+        # Вставка данных о продукте в базу данных ChromaDB
+        pass
+
+    def fetch_product_cards(self):
+        # Получение данных о продуктах из базы данных ChromaDB
+        pass
